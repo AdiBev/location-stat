92a3eacde9cfb1dac2cc7e304695a284192e647f
Aditya Bevoor <adityabev@gmail.com>
2023-05-10 21:26:39 +0100

    Update vite config to have alias, build dir and server to open

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: "build",
+  },
   test: {
     globals: true,
     environment: "jsdom",
